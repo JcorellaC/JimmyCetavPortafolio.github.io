6c82e525e1f8457fa1b81492e2c912175be19615
JimmyCetav <jimmysaw24@gmail.com>
2015-04-02 00:06:38 -0600

    _asignacion de iconos por js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ var portafolio = {}; // crea una variable global contenedora
         portafolio.administrador = function(){
 
         	var links = [
-                new link('facebook'),
-                new link('twitter'), 
-                new link('github')
+                new link('facebook-square'),
+                new link('twitter-square'), 
+                new link('github-square'),
+                new link('linkedin-square')
             ];
 
             function socialNetworks(arreglo){
@@ -13,20 +14,14 @@ var portafolio = {}; // crea una variable global contenedora
                 var contenido = '<ul class="links">';
                 
                 for(var k=0; k < arreglo.length; k++){
-                         contenido += '<li class="network'+k+'"><a href="#">'+arreglo[k].net+'</a></li>';
-                } 
-             
+                         contenido += '<li class="network network'+k+'"><span class="fa fa-'+arreglo[k].net+' "></span><a class="networkLink" href="#"></a></li>';
+                }
+
                 contenido += '</ul>';
-                
                 document.getElementById('contenedor-links').innerHTML=contenido;
             }
 
-
            return{
-
-               
-               
-              
                mostrarNetworks: function(){
                     socialNetworks(links);
                }
@@ -36,7 +31,6 @@ var portafolio = {}; // crea una variable global contenedora
 
         function link(n){
             this.net = n;
-        
         }
 
        portafolio.administrador.mostrarNetworks();
