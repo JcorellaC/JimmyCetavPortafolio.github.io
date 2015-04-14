f52314f3b74fefb14c533258aecba2e41b5f4dc0
JimmyCetav <jimmysaw24@gmail.com>
2015-04-14 12:46:12 -0600

    creacion de un sistema de validacion por medio de js, llamado de un ajax con jquery, sistema de funciones hechas para darle algunos procesos al web site

---
+++ js/myScript.js
@@ -0,0 +1,63 @@
+var portafolio = {}; 
+
+        portafolio.administrador = function(){
+          window.onload = function () {
+            document.formularioContacto.addEventListener('submit', validarFormulario);
+          }
+
+          var email = document.formularioContacto.email;
+          console.log (email);
+          var todoCorrecto = true;
+          var formulario = document.formularioContacto;
+          
+          //Un closure es una función que tiene acceso al ámbito padre, incluso después de la función madre se ha cerrado.
+
+         function validarFormulario(evObject) {
+          evObject.preventDefault();
+
+            for (var i=0; i<formulario.length; i++) {
+                if(formulario[i].type =='text') {
+                    if (formulario[i].value == null || formulario[i].value.length == 0 || /^\s*$/.test(formulario[i].value)){
+                        alert (formulario[i].name+ ' no puede estar vacío o contener sólo espacios en blanco');
+                        todoCorrecto=false;
+                    }
+                }
+            }
+            if (email.value == '' || !/\S+@\S+\.\S+/.test(email.value)){
+              alert ('Please enter a valid email address.');
+              todoCorrecto=false;
+            }
+
+  if (todoCorrecto ==true) {
+    formulario.submit();
+  }
+}
+
+ function verAyuda(ayuda) {
+  document.getElementById('help').innerHTML = ayuda;
+}
+
+function crearAyuda(ayuda) {
+  return function() {
+    verAyuda(ayuda);
+  };
+}
+
+function infoAyuda() {
+  var textoAyuda = [
+      {'id': 'email', 'ayuda': 'Your e-mail address'},
+      {'id': 'name', 'ayuda': 'Your full name'},
+      {'id': 'mesage', 'ayuda': 'That you need me to say'}
+    ];
+
+  for (var i = 0; i < textoAyuda.length; i++) {
+    var objeto = textoAyuda[i];
+    document.getElementById(objeto.id).onfocus = crearAyuda(objeto.ayuda);
+  }
+}
+
+infoAyuda();
+}();
+
+//Modulo es el entorno de programación orientado a objetos,donde las variables internas las llamamos propiedades 
+//mientras que a las funciones, las llamamos métodos.
--- js/script.js
+++ js/script.js
@@ -1,36 +1,128 @@
-var portafolio = {}; // crea una variable global contenedora
+$(document).ready(function(){
 
-        portafolio.administrador = function(){
+        $(function(){
+        $.blockUI();
+        setTimeout($.unblockUI, 400);
+        $.ajax({
 
-        	var links = [
-                new link('facebook-square'),
-                new link('twitter-square'), 
-                new link('github-square'),
-                new link('linkedin-square')
-            ];
+            type: "GET",
+            url: "../js/proyectos.json",
+            dataType: "json",
+            success:function(data){
+                    
+                listaProjects(data.proyectos);
 
-            function socialNetworks(arreglo){
-            
-                var contenido = '<ul class="links">';
+            },
+
+            error:function(jqXHR, textStatus, errorThrown) {
+
+                console.log("Text Status:" +textStatus+"\nError:"+errorThrown);
                 
-                for(var k=0; k < arreglo.length; k++){
-                         contenido += '<li class="network network'+k+'"><span class="fa fa-'+arreglo[k].net+' "></span><a class="networkLink" href="#"></a></li>';
-                }
+            },
+            complete:function(){
+                        
+            },
+ 
+        });
 
-                contenido += '</ul>';
-                document.getElementById('contenedor-links').innerHTML=contenido;
+        function listaProjects(proyectos){
+            var projectlist = new Array;
+
+            for(var i = 0; i < proyectos.length; i++){
+
+
+                projectlist[i] = 
+                //<html--
+                    '<div class="show show-project ">' +
+                        '<img class="img1" src="../img/' + proyectos[i].imagen1 + '.jpg">' +
+                        '<div class="mask see-project">' +
+                            '<img class="img1" src="../img/' + proyectos[i].imagen1 + '.jpg">' +
+                            '<img class="img2" src="../img/' + proyectos[i].imagen2 + '.jpg">' +
+                            '<img class="img3" src="../img/' + proyectos[i].imagen3 + '.jpg">' +
+                            '<img class="clienteImg" src="../img/' + proyectos[i].imagenCliente + '.jpg">' +
+                            '<h2>' + proyectos[i].ID + '</h2>'+
+                            '<p class="description-text">' + proyectos[i].descripcion +'</p>' +
+                            '<p class="cliente-text">' + proyectos[i].cliente + '</p>' +
+                            '<p class="more-text">' + proyectos[i].more + '</p>' +
+                            '<a href="#one-project" class="info buttom">Click to view more</a>' +
+                            '<a class="hidden-link info buttom" href="../proyectos/' + proyectos[i].viewPage + '">View complete</a>' +
+                            
+                        '</div>' +
+
+                    '</div>';
+                    
+                //--html>
+                
             }
 
-           return{
-               mostrarNetworks: function(){
-                    socialNetworks(links);
-               }
-           }
+            var datos = '';
+            $('#projects').on('click', '.see-project', function(){
+                var imagen1 = $(this).children('img.img1').attr('src');
+                var imagen2 = $(this).children('img.img2').attr('src');
+                var imagen3 = $(this).children('img.img3').attr('src');
+                var imagenCliente = $(this).children('img.clienteImg').attr('src');
+                var titulo = $(this).children('h2.titulo').text();
+                var texto1 = $(this).children('p.description-text').text();
+                var texto2 = $(this).children('p.cliente-text').text();
+                var texto3 = $(this).children('p.more-text').text();
+                var link = $(this).children('a.hidden-link').attr('href');
 
-        }();
+                datos =
+                //<html--
+                    '<div class="specific-container">' + 
+                        '<h2>' + titulo + '</h2>'+
+                        '<section class="tabs">'+
+                            '<input id="tab-1" type="radio" name="radio-set" class="tab-selector-1" checked="checked"/>' +
+                            '<label for="tab-1" class="tab-label-1">Project</label>' +
+                    
+                            '<input id="tab-2" type="radio" name="radio-set" class="tab-selector-2"/>' +
+                            '<label for="tab-2" class="tab-label-2">Customer</label>' +
+                    
+                            '<input id="tab-3" type="radio" name="radio-set" class="tab-selector-3"/>' +
+                            '<label for="tab-3" class="tab-label-3">View</label>' +
+                            '<div class="clear-shadow"></div>' +
+                
+                            '<div class="content">' +
+                                '<div class="content-1">' +
+                                    '<h2>' + titulo + '</h2>' +
+                                    '<p>' + texto1 + '</p>' +
+                                    '<h3>How we work</h3>' +
+                                    '<img class="proyect-image" src="' + imagen1 + '">' +
+                                    '<img class="proyect-image" src="' + imagen2 + '">' +
+                                    '<img class="proyect-image" src="' + imagen3 + '">' +
+                                '</div>' +
+                                '<div class="content-2">' +
+                                    '<h2>Customer</h2>' +
+                                    '<p>' + texto2 + '</p>' +
+                                    '<h3>View</h3>' +
+                                    '<img class="proyect-image" src="' + imagenCliente + '">' +
+                                    '</div>' +
+                                '<div class="content-3">' +
+                                    '<h2>More about</h2>' +
+                                    '<p>' + texto3 + '</p>' +
+                                    '<a class="hidden-link info buttom" href="' + link + '">View complete</a>' +
+                                '</div>' +
+                            '</div>' +
+                        '</section>' +
+                    '</div>';
+                //--html>
 
-        function link(n){
-            this.net = n;
+                $('#one-project').html(datos);
+            });
+            
+
+            $('#projects').append(projectlist);
+            
+
+
         }
 
-       portafolio.administrador.mostrarNetworks();
+
+    });
+
+});
+
+
+
+
+
